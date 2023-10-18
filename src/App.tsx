8766871c24c3807c6e57fc87f1dc2892dda0be01
Sebastian Sandoval <sebassandoval@hotmail.com>
2023-10-18 11:25:58 -0500

    Add initial configuration

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from '@mui/material'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Button variant="contained">Text</Button>
+    </>
+  )
+}
+
+export default App
